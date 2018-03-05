ff9b6ca0e71b252ef16d1206ff43d3475b7a4a19
Slater <megan.e.slater@gmail.com>
2018-03-04 20:01:21 -0800

    basic routing example

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 function changeRoutes(routeName) {
-  window.location.assign(routeName);
+  history.pushState({}, '', routeName);
+  while (document.body.firstChild) {
+    document.body.removeChild(document.body.firstChild);
+  }
+  return pages[routeName]();
 }
 
-window.addEventListener('load', () => {
+function renderCats() {
+  const button = document.createElement('button');
+  button.innerText = 'Go Home';
+  button.addEventListener('click', () => changeRoutes(''));
+  document.body.appendChild(button);
+}
+
+function renderHome() {
   const header = document.createElement('h1');
   header.innerText = "Hello World!";
   document.body.appendChild(header);
@@ -10,4 +21,11 @@ window.addEventListener('load', () => {
   button.innerText = "go to cats";
   button.addEventListener('click', () => changeRoutes('cats'));
   document.body.appendChild(button);
-});
+}
+
+const pages = {
+  cats: renderCats,
+  '': renderHome,
+}
+
+window.addEventListener('load', () => renderHome());
