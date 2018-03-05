9a666b1b3ddb26f082ecc1e09045825d9c499306
Slater <megan.e.slater@gmail.com>
2018-03-04 20:23:50 -0800

    make convenience methods and listen to popstate event so that routing is consistent

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 function changeRoutes(routeName) {
-  history.pushState({}, '', routeName);
-  while (document.body.firstChild) {
-    document.body.removeChild(document.body.firstChild);
-  }
-  return pages[routeName]();
+  history.pushState({ arbitraryState: true }, '', routeName);
+  window.dispatchEvent(new Event('popstate'));
+}
+
+function createElement(tagName, options) {
+  const elm = document.createElement(tagName);
+  Object.keys(options).forEach((option) => {
+    elm[option] = options[option];
+  });
+  return elm;
 }
 
 function renderCats() {
-  const button = document.createElement('button');
-  button.innerText = 'Go Home';
-  button.addEventListener('click', () => changeRoutes(''));
+  const button = createElement('button', {
+    innerText: 'Go Home',
+    onclick: () => changeRoutes('/'),
+  });
   document.body.appendChild(button);
 }
 
 function renderHome() {
-  const header = document.createElement('h1');
-  header.innerText = "Hello World!";
+  const header = createElement('h1', {
+    innerText: 'Hello World!',
+  })
   document.body.appendChild(header);
-  const button = document.createElement('button');
-  button.innerText = "go to cats";
-  button.addEventListener('click', () => changeRoutes('cats'));
+  const button = createElement('button', {
+    innerText: 'go to cats',
+    onclick: () => changeRoutes('/cats'),
+  });
   document.body.appendChild(button);
 }
 
 const pages = {
-  cats: renderCats,
-  '': renderHome,
+  '/cats': renderCats,
+  '/': renderHome,
 }
 
 window.addEventListener('load', () => renderHome());
+window.onpopstate = (e) => {
+  while (document.body.firstChild) {
+    document.body.removeChild(document.body.firstChild);
+  }
+  return pages[e.currentTarget.location.pathname]();
+}
